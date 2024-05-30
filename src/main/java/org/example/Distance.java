be24779299d30df52d0d49066643b40dc5292037
Birger <birger@luland.de>
2024-05-30 17:52:41 +0200

    javadoc kommentare hinzugefügt

--- src/main/java/org/example/Distance.java
+++ src/main/java/org/example/Distance.java
@@ -2,8 +2,16 @@ package org.example;
 
 import java.util.*;
 
+/**
+ * implementiert die methode distance()
+ */
 public class Distance {
 
+    /**
+     * konvertiert den Graphen von einem Set aus Edges zu einer Map aus Knoten und dessen Nachbarn
+     * @param graph Set aus Node Tupeln, zwischen denen Edges verlaufen.
+     * @return Map aus Nodes, die auf ihre jeweiligen Nachbarn zeigen
+     */
     private static Map<Object, Set<Object>> getNodes(Set<List<Object>> graph) {
         Map<Object, Set<Object>> nodes = new HashMap<>();
 
@@ -25,8 +33,15 @@ public class Distance {
         return nodes;
     }
 
-    public static int distance(Object source, Object destination, Set<List<Object>> graphList) {
-        Map<Object, Set<Object>> nodes = getNodes(graphList);
+    /**
+     * rechnet die Distanz zwischen source und
+     * @param source ist das Objekt, von dem die Suche aus los geht
+     * @param destination ist das Objekt, nach dem gesucht wird
+     * @param graph Set aus Node Tupeln, zwischen denen Edges verlaufen
+     * @return Anzahl an Kanten zwischen source und destination, wenn ein weg gefunden wurde. falls kein weg existiert wird -1 zurückgegeben.
+     */
+    public static int distance(Object source, Object destination, Set<List<Object>> graph) {
+        Map<Object, Set<Object>> nodes = getNodes(graph);
         Map<Object, Integer> distance = new HashMap<>();
 
         Queue<Object> queue = new LinkedList<>();
