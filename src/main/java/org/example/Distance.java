ce63d649149afa805b7c9c848ebbd2b72c3d88c1
Birger <birger@luland.de>
2024-05-30 18:27:04 +0200

    Nodes Klasse, zur übersichtlichkeit, als innere Klasse hinzugefügt.

--- src/main/java/org/example/Distance.java
+++ src/main/java/org/example/Distance.java
@@ -3,32 +3,27 @@ package org.example;
 import java.util.*;
 
 /**
- * implementiert die methode distance()
+ * implementiert die Methode distance()
  */
 public class Distance {
 
     /**
-     * konvertiert den Graphen von einem Set aus Edges zu einer Map aus Knoten und dessen Nachbarn
+     * konvertiert den Graphen von einem Set aus Edges zu einer Map aus Objekten und dazugehörigen Nodes
      * @param graph Set aus Node Tupeln, zwischen denen Edges verlaufen.
-     * @return Map aus Nodes, die auf ihre jeweiligen Nachbarn zeigen
+     * @return Map aus den Objekten als Keys, und Nodes als deren Values
      */
-    private static Map<Object, Set<Object>> getNodes(Set<List<Object>> graph) {
-        Map<Object, Set<Object>> nodes = new HashMap<>();
+    private static Map<Object, Node> getNodes(Set<List<Object>> graph) {
+        Map<Object, Node> nodes = new HashMap<>();
 
         graph.forEach(edge -> {
             edge.forEach(object -> {
                 if (!nodes.containsKey(object)) {
-                    nodes.put(object, new HashSet<>());
+                    nodes.put(object, new Node(object));
                 }
             });
 
-            edge.forEach(node -> {
-                edge.forEach(otherNode -> {
-                    if (edge != otherNode) {
-                        nodes.get(node).add(otherNode);
-                    }
-                });
-            });
+            List<Node> edgeOfNodes = edge.stream().map(nodes::get).toList();
+            edgeOfNodes.forEach(node -> node.addNeighbors(edgeOfNodes));
         });
         return nodes;
     }
@@ -41,25 +36,47 @@ public class Distance {
      * @return Anzahl an Kanten zwischen source und destination, wenn ein weg gefunden wurde. falls kein weg existiert wird -1 zurückgegeben.
      */
     public static int distance(Object source, Object destination, Set<List<Object>> graph) {
-        Map<Object, Set<Object>> nodes = getNodes(graph);
-        Map<Object, Integer> distance = new HashMap<>();
+        Map<Object, Node> nodes = getNodes(graph);
 
-        Queue<Object> queue = new LinkedList<>();
-        queue.add(source);
-        distance.put(source, 0);
+        Queue<Node> queue = new LinkedList<>();
+
+        Node sourceNode = nodes.get(source);
+        sourceNode.distance = 0;
+        queue.add(sourceNode);
 
         while (!queue.isEmpty()) {
-            Object node = queue.poll();
-            for (Object neighbor : nodes.get(node)) {
-                if (!distance.containsKey(neighbor)) {
-                    if (neighbor == destination) {
-                        return distance.get(node) + 1;
+            Node node = queue.poll();
+            for (Node neighbor : node.neighbors) {
+                if (!neighbor.marked) {
+                    if (neighbor.object == destination) {
+                        return node.distance + 1;
                     }
-                    distance.put(neighbor, distance.get(node) + 1);
+                    neighbor.distance = node.distance + 1;
                     queue.add(neighbor);
                 }
             }
         }
         return -1;
+    }
+
+    private static class Node {
+        final Object object;
+        Set<Node> neighbors;
+        int distance;
+        boolean marked;
+
+        public Node(Object object) {
+            this.object = object;
+            neighbors = new HashSet<>();
+            marked = false;
+        }
+
+        public void addNeighbors(List<Node> neighbors){
+            neighbors.forEach(node -> {
+                if (this != node){
+                    this.neighbors.add(node);
+                }
+            });
+        }
     }
 }
